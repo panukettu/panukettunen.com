3028150bb221fc2225aa0adba415304d7dbc11be
panu.kettunen <panu.t.kettunen@gmail.com>
2018-10-24 15:04:56 +0300

    disable the current page link on navigation

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 export const NavLink = ({ to, children }) => {
   let renderLink = true
   if (typeof window !== `undefined`) {
-    renderLink = to === window.location.pathname
+    renderLink = to !== window.location.pathname
   }
 
   return renderLink ? <Link to={to}>{children}</Link> : <span>{children}</span>
